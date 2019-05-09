2c64bf902a4d43718ad44565f0053e40d856fc07
Bowen <bowenxu@me.com>
2019-05-09 10:12:41 -0400

    Spring data rest only need ntity and repository

--- src/main/java/com/box/springboot/cruddemo/dao/EmployeeRepository.java
+++ src/main/java/com/box/springboot/cruddemo/dao/EmployeeRepository.java
@@ -3,10 +3,12 @@ package com.box.springboot.cruddemo.dao;
 import com.box.springboot.cruddemo.entity.Employee;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 /**
  * EmployeeRepository
  */
+//@RepositoryRestResource(path = "members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
     
